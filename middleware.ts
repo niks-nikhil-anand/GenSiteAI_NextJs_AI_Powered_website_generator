2a528486b4ab99f3f52d6e14121743dcc57dc8a3
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-03-05 23:29:11 +0530

    Updated the middleware

--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,27 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware(async (auth, request) => {
-  if (!isPublicRoute(request)) {
-    await auth.protect()
-  }
-})
 const isPublicRoute = createRouteMatcher([
   '/sign-in(.*)',
   '/sign-up(.*)',
   '/'
 ]);
 
+const isProtectedRoute = createRouteMatcher([
+  '/workplace(.*)',
+  "/testApi"
+]);
 
+export default clerkMiddleware( async (auth , req) => {
+  if (isProtectedRoute(req)) await auth.protect()
 
+})
 
 export const config = {
   matcher: [
-    // Explicitly protect the `/workplace` route
-    '/workplace',
     // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 };
-
