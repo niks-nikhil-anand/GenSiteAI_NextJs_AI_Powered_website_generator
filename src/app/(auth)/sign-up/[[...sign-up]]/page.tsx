5159ea5adba92f904cddceedcf94542f54fa10cb
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-03-05 23:08:30 +0530

    fix typo

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from '@clerk/nextjs'
 export default function Page() {
     return (
         <div className='flex justify-center items-center min-h-[90vh]'>
-            <SignUp />
+            <SignUp afterSignOutUrl="/" />
         </div>
     )
 
